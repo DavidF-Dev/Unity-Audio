7cbf7fc4657b7b6416ef68a787704e36722f50b8
David F <account@davidfdev.com>
2022-05-05 15:26:40 +1000

    Update SoundEffect.cs

--- Runtime/SoundEffect.cs
+++ Runtime/SoundEffect.cs
@@ -2,11 +2,13 @@
 // Purpose: Create a sound effect that can be played at any time.
 // Created by: DavidFDev
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.Audio;
+using Random = UnityEngine.Random;
 
 namespace DavidFDev.Audio
 {
@@ -16,13 +18,7 @@ namespace DavidFDev.Audio
     [CreateAssetMenu(menuName = "DavidFDev/Audio/Sound Effect")]
     public sealed class SoundEffect : ScriptableObject
     {
-        #region Fields
-
-        private int _previousClipIndex = -1;
-
-        #endregion
-
-        #region Constructors
+        #region Static Methods
 
         /// <summary>
         ///     Create a new sound effect instance via code.
@@ -37,22 +33,44 @@ namespace DavidFDev.Audio
         /// <param name="loop">Whether the audio playback should loop.</param>
         /// <param name="priority">Priority of the audio playback [0 - 256].</param>
         /// <param name="stereoPan">Pan the location of a stereo or mono audio playback [-1.0 (left) - 1.0 (right)].</param>
-        /// <param name="spatialBlend">Amount that the audio playback is affected by spatialisation calculations [0.0 (2D) - 1.0 (3D)].</param>
-        public SoundEffect(AudioClip[] clips, bool smartRandom = true, AudioMixerGroup output = null,
+        /// <param name="spatialBlend">
+        ///     Amount that the audio playback is affected by spatialisation calculations [0.0 (2D) - 1.0
+        ///     (3D)].
+        /// </param>
+        [PublicAPI]
+        [NotNull]
+        public static SoundEffect Create([NotNull] AudioClip[] clips, bool smartRandom = true,
+            [CanBeNull] AudioMixerGroup output = null,
             float minVolume = 1f, float maxVolume = 1f, float minPitch = 1f, float maxPitch = 1f,
             bool loop = false, int priority = 128, float stereoPan = 0f, float spatialBlend = 0f)
         {
-            Clips = clips;
-            SmartRandom = smartRandom;
-            Output = output;
-            MinVolume = Mathf.Clamp01(minVolume);
-            MaxVolume = Mathf.Clamp01(maxVolume);
-            MinPitch = Mathf.Clamp(minPitch, -3f, 3f);
-            MaxPitch = Mathf.Clamp(maxPitch, -3f, 3f);
-            Loop = loop;
-            Priority = Mathf.Clamp(priority, 0, 256);
-            StereoPan = Mathf.Clamp(stereoPan, -1f, 1f);
-            SpatialBlend = Mathf.Clamp01(spatialBlend);
+            var instance = CreateInstance<SoundEffect>();
+            instance.Clips = clips;
+            instance.SmartRandom = smartRandom;
+            instance.Output = output;
+            instance.MinVolume = Mathf.Clamp01(minVolume);
+            instance.MaxVolume = Mathf.Clamp01(maxVolume);
+            instance.MinPitch = Mathf.Clamp(minPitch, -3f, 3f);
+            instance.MaxPitch = Mathf.Clamp(maxPitch, -3f, 3f);
+            instance.Loop = loop;
+            instance.Priority = Mathf.Clamp(priority, 0, 256);
+            instance.StereoPan = Mathf.Clamp(stereoPan, -1f, 1f);
+            instance.SpatialBlend = Mathf.Clamp01(spatialBlend);
+            return instance;
+        }
+
+        #endregion
+
+        #region Fields
+
+        private int _previousClipIndex = -1;
+
+        #endregion
+
+        #region Constructors
+
+        private SoundEffect()
+        {
         }
 
         #endregion
@@ -62,79 +80,109 @@ namespace DavidFDev.Audio
         /// <summary>
         ///     A random clip is chosen when the sound effect is played.
         /// </summary>
-        [field: Tooltip("A random clip is chosen when the sound effect is played."), SerializeField]
-        [PublicAPI, NotNull]
-        public AudioClip[] Clips { get; private set; }
+        [field: Tooltip("A random clip is chosen when the sound effect is played.")]
+        [field: SerializeField]
+        [PublicAPI]
+        [NotNull]
+        public AudioClip[] Clips { get; private set; } = Array.Empty<AudioClip>();
 
         /// <summary>
         ///     Prevents the same clip from being played twice in a row.
         /// </summary>
-        [field: Tooltip("Prevents the same clip from being played twice in a row."), SerializeField]
+        [field: Tooltip("Prevents the same clip from being played twice in a row.")]
+        [field: SerializeField]
         [PublicAPI]
         public bool SmartRandom { get; private set; }
 
         /// <summary>
         ///     Group that the audio playback should output to.
         /// </summary>
-        [field: Space, SerializeField]
-        [PublicAPI, CanBeNull]
+        [field: Space]
+        [field: SerializeField]
+        [PublicAPI]
+        [CanBeNull]
         public AudioMixerGroup Output { get; private set; }
 
         /// <summary>
         ///     Minimum possible volume of the audio playback [0.0 - 1.0]. Volume is chosen randomly.
         /// </summary>
-        [field: Header("Volume"), SerializeField, Range(0f, 1f)]
+        [field: Header("Volume")]
+        [field: SerializeField]
+        [field: Range(0f, 1f)]
         [PublicAPI]
         public float MinVolume { get; private set; }
 
         /// <summary>
         ///     Maximum possible volume of the audio playback [0.0 - 1.0]. Volume is chosen randomly.
         /// </summary>
-        [field: SerializeField, Range(0f, 1f)]
+        [field: SerializeField]
+        [field: Range(0f, 1f)]
         [PublicAPI]
         public float MaxVolume { get; private set; }
 
         /// <summary>
         ///     Minimum pitch of the audio playback [-3.0 - 3.0]. Pitch is chosen randomly.
         /// </summary>
-        [field: Header("Pitch"), SerializeField, Range(-3f, 3f)]
+        [field: Header("Pitch")]
+        [field: SerializeField]
+        [field: Range(-3f, 3f)]
         [PublicAPI]
         public float MinPitch { get; private set; }
 
         /// <summary>
         ///     Maximum pitch of the audio playback [-3.0 - 3.0]. Pitch is chosen randomly.
         /// </summary>
-        [field: SerializeField, Range(-3f, 3f)]
+        [field: SerializeField]
+        [field: Range(-3f, 3f)]
         [PublicAPI]
         public float MaxPitch { get; private set; }
 
         /// <summary>
         ///     Whether the audio playback should loop.
         /// </summary>
-        [field: Space, SerializeField]
+        [field: Space]
+        [field: SerializeField]
         [PublicAPI]
         public bool Loop { get; private set; }
 
         /// <summary>
         ///     Priority of the audio playback [0 - 256].
         /// </summary>
-        [field: SerializeField, Range(0, 256)]
+        [field: SerializeField]
+        [field: Range(0, 256)]
         [PublicAPI]
         public int Priority { get; private set; }
 
         /// <summary>
         ///     Pan the location of a stereo or mono audio playback [-1.0 (left) - 1.0 (right)].
         /// </summary>
-        [field: SerializeField, Range(-1f, 1f)]
+        [field: SerializeField]
+        [field: Range(-1f, 1f)]
         [PublicAPI]
         public float StereoPan { get; private set; }
 
         /// <summary>
         ///     Amount that the audio playback is affected by spatialisation calculations [0.0 (2D) - 1.0 (3D)].
         /// </summary>
-        [field: SerializeField, Range(0f, 1f)]
+        [field: SerializeField]
+        [field: Range(0f, 1f)]
         [PublicAPI]
         public float SpatialBlend { get; private set; }
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Reset()
+        {
+            Clips = Array.Empty<AudioClip>();
+            SmartRandom = true;
+            MinVolume = 1f;
+            MaxVolume = 1f;
+            MinPitch = 1f;
+            MaxPitch = 1f;
+            Priority = 128;
+        }
 
         #endregion
 
@@ -145,7 +193,8 @@ namespace DavidFDev.Audio
         /// </summary>
         /// <param name="position">Position of the audio playback in 3D world-space.</param>
         /// <returns>Instance for controlling audio playback.</returns>
-        [PublicAPI, CanBeNull]
+        [PublicAPI]
+        [CanBeNull]
         public Playback Play(Vector3 position = default)
         {
             return Audio.PlaySfx(this, position);
@@ -187,16 +236,6 @@ namespace DavidFDev.Audio
             _previousClipIndex = clipIndex;
 
             return Clips[clipIndex];
-        }
-
-        private void Reset()
-        {
-            SmartRandom = true;
-            MinVolume = 1f;
-            MaxVolume = 1f;
-            MinPitch = 1f;
-            MaxPitch = 1f;
-            Priority = 128;
         }
 
         #endregion
